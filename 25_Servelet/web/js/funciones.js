76ead383c2f82d474fa0b47cae49c6084ba7d053
SaraIxshel <si.cortes.h@edu.utc.mx>
2022-02-16 22:20:08 -0600

    SERVIDOR

---
+++ 25_Servelet/web/js/funciones.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+*{
+    background-color: #d4f4a4;
+}
